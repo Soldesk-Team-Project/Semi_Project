d4c820994833ba95cb30e6ef1b8234f699127282
Junseok <oh971021@gmail.com>
2021-12-16 16:14:25 +0900

    Soldesk Semi Project : Update Basic Setting File

---
+++ TodayMenu/src/main/java/com/soldesk/semi/hc/HomeController.java
@@ -0,0 +1,24 @@
+package com.soldesk.semi.hc;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HomeController")
+public class HomeController extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setAttribute("slidePage", "home/homeSlide.jsp");
+		request.setAttribute("articlePage", "home/homeArticle.jsp");
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		
+	}
+
+}
