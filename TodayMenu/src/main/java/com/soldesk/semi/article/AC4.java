94b4bf2b17d186214bc4959e12865777c574c4e6
Junseok <oh971021@gmail.com>
2021-12-30 15:51:39 +0900

    Soldesk Semi Project : Update Article Page

--- TodayMenu/src/main/java/com/soldesk/semi/article/AC4.java
+++ TodayMenu/src/main/java/com/soldesk/semi/article/AC4.java
@@ -7,11 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.soldesk.semi.account.AccountDAO;
+
 @WebServlet("/AC4")
 public class AC4 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		
+		AccountDAO.loginCheck(request);
+		request.setAttribute("contentPage", "article/article4.jsp");
+		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
